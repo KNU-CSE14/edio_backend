783d5227fb93dedbfe583ff30d905ac6331c9546
jsjouo249 <jsjouo249@naver.com>
2024-08-28 21:53:37 +0900

    add/ edio backend initial setting with spring boot

---
+++ src/test/java/com/edio/EdioBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdioBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
