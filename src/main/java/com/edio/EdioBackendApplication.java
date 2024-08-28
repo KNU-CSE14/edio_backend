70a61fe8f832766a45d82b1ff31eee75455e23d0
jsjouo249 <jsjouo249@naver.com>
2024-08-28 21:53:37 +0900

    add/ edio backend initial setting with spring boot

---
+++ src/main/java/com/edio/EdioBackendApplication.java
@@ -0,0 +1,13 @@
+package com.edio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdioBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdioBackendApplication.class, args);
+	}
+
+}
