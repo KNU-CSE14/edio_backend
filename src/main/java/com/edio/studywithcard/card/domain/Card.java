af6101b631c5fdc69e2c6959e5794d6c8d9cf5d8
YUNHWAN <629jyh7@naver.com>
2024-10-18 15:52:54 +0900

    add/ MySQL 연결 및 JPA 설정

--- src/main/java/com/edio/studywithcard/card/domain/Card.java
+++ src/main/java/com/edio/studywithcard/card/domain/Card.java
@@ -1,4 +1,21 @@
 package com.edio.studywithcard.card.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
 public class Card {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
 }
--- src/main/java/com/edio/studywithcard/card/repository/CardRepository.java
+++ src/main/java/com/edio/studywithcard/card/repository/CardRepository.java
@@ -1,6 +1,7 @@
 package com.edio.studywithcard.card.repository;
 
 import com.edio.studywithcard.card.domain.Card;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CardRepository{
+public interface CardRepository extends JpaRepository<Card, Long> {
 }
