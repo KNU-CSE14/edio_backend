df9020b58d0e3929785c99684523f88836d03e87
YUNHWAN <629jyh7@naver.com>
2024-10-18 15:52:54 +0900

    add/ MySQL 연결 및 JPA 설정

--- src/main/java/com/edio/EdioBackendApplication.java
+++ src/main/java/com/edio/EdioBackendApplication.java
@@ -1,5 +1,6 @@
 package com.edio;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EdioBackendApplication {
 
 	public static void main(String[] args) {
+
+		// .env 파일 로드
+		Dotenv dotenv = Dotenv.configure().load();
+
+		// 환경 변수 값으로 시스템 속성 설정
+		System.setProperty("spring.datasource.username", dotenv.get("DB_USERNAME"));
+		System.setProperty("spring.datasource.password", dotenv.get("DB_PASSWORD"));
+		System.setProperty("spring.datasource.url", dotenv.get("DB_URL"));
+
 		SpringApplication.run(EdioBackendApplication.class, args);
+
 	}
 
 }
--- src/main/java/com/edio/studywithcard/card/service/CardServiceImpl.java
+++ src/main/java/com/edio/studywithcard/card/service/CardServiceImpl.java
@@ -1,7 +1,15 @@
 package com.edio.studywithcard.card.service;
 
+import com.edio.studywithcard.card.domain.Card;
+import com.edio.studywithcard.card.repository.CardRepository;
 import org.springframework.stereotype.Service;
 
 @Service
-public class CardServiceImpl {
+public class CardServiceImpl implements CardService{
+
+    private final CardRepository cardRepository;
+
+    public CardServiceImpl(CardRepository cardRepository) {
+        this.cardRepository = cardRepository;
+    }
 }
