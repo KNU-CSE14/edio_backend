08f556fa889b6b9e74ed94e5dbdde0eb5b294fe9
jsjouo249 <jsjouo249@naver.com>
2024-10-24 17:58:02 +0900

    feat/test에 db 설정 추가

--- src/test/java/com/edio/EdioBackendApplicationTests.java
+++ src/test/java/com/edio/EdioBackendApplicationTests.java
@@ -1,10 +1,22 @@
 package com.edio;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class EdioBackendApplicationTests {
+	@BeforeAll
+	static void setUp() {
+		// .env 파일 로드
+		Dotenv dotenv = Dotenv.configure().load();
+
+		// 환경 변수 값으로 시스템 속성 설정
+		System.setProperty("spring.datasource.username", dotenv.get("DB_USERNAME"));
+		System.setProperty("spring.datasource.password", dotenv.get("DB_PASSWORD"));
+		System.setProperty("spring.datasource.url", dotenv.get("DB_URL"));
+	}
 
 	@Test
 	void contextLoads() {
