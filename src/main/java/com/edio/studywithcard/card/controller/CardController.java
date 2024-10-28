38be084b2bbde83d5f0a6310dbea7d0c32485cfb
YUNHWAN <629jyh7@naver.com>
2024-10-28 15:14:47 +0900

    feat: 데이터베이스 health check 코드 추가

--- src/main/java/com/edio/studywithcard/card/controller/CardController.java
+++ src/main/java/com/edio/studywithcard/card/controller/CardController.java
@@ -4,10 +4,17 @@ import com.edio.common.model.response.SwaggerCommonResponses;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 @Tag(name = "Card", description = "Card 관련 API")
 @RestController
@@ -21,5 +28,21 @@ public class CardController {
     @SwaggerCommonResponses //Swagger 공통 응답 어노테이션
     public Long getCard(@Parameter(required = true, description = "Card ID") @PathVariable long cardId){
         return cardId;
+    }
+
+    @Autowired
+    private DataSource dataSource;
+
+    @GetMapping("/health/db")
+    public ResponseEntity<String> checkDatabaseConnection() {
+        try (Connection connection = dataSource.getConnection()) {
+            if (connection != null && !connection.isClosed()) {
+                return ResponseEntity.ok("Database connection is OK");
+            } else {
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Database connection is not established");
+            }
+        } catch (SQLException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to connect to database: " + e.getMessage());
+        }
     }
 }
