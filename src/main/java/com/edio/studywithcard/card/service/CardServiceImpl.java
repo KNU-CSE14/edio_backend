84bf3e3e4b84bd5e420c9ee03651a24a6a1479da
YUNHWAN <629jyh7@naver.com>
2024-10-28 15:33:49 +0900

    feat: 데이터베이스 연결 및 조회 테스트 코드 추가

--- src/main/java/com/edio/studywithcard/card/service/CardServiceImpl.java
+++ src/main/java/com/edio/studywithcard/card/service/CardServiceImpl.java
@@ -4,6 +4,9 @@ import com.edio.studywithcard.card.domain.Card;
 import com.edio.studywithcard.card.repository.CardRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class CardServiceImpl implements CardService{
 
@@ -11,5 +14,14 @@ public class CardServiceImpl implements CardService{
 
     public CardServiceImpl(CardRepository cardRepository) {
         this.cardRepository = cardRepository;
+    }
+
+
+    @Override
+    public List<String> getCardTest() {
+        // Card 엔티티에서 title만 추출하여 반환
+        return cardRepository.findAll().stream()
+                .map(Card::getTitle)  // title 필드가 있다고 가정
+                .collect(Collectors.toList());
     }
 }
