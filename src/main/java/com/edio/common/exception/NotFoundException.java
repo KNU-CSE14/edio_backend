80715fd244ea950ac07d0f00f31ee8ae38ff8a7d
YUNHWAN <629jyh7@naver.com>
2024-11-06 13:33:51 +0900

    feat: 예외 메시지 형식 클래스 엔티티 내용으로 수정

--- src/main/java/com/edio/common/exception/NotFoundException.java
+++ src/main/java/com/edio/common/exception/NotFoundException.java
@@ -5,11 +5,11 @@ import org.springframework.http.HttpStatus;
 
 @Getter
 public class NotFoundException extends BaseException{
-    public NotFoundException(String serviceName, Long id){
-        super(HttpStatus.NOT_FOUND, String.format("%s not found: %d", serviceName, id));
+    public <T> NotFoundException(Class<T> entityClass, Long id){
+        super(HttpStatus.NOT_FOUND, String.format("%s not found: %d", entityClass.getSimpleName(), id));
     }
 
-    public NotFoundException(String serviceName, Object message){
-        super(HttpStatus.NOT_FOUND, String.format("%s not found: %s", serviceName, message));
+    public <T> NotFoundException(Class<T> entityClass, Object message){
+        super(HttpStatus.NOT_FOUND, String.format("%s not found: %s", entityClass.getSimpleName(), message));
     }
 }
