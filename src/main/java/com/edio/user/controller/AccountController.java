f50b7dc0b92aa00ea6ecb91cb2c7c307ac08432b
YUNHWAN <629jyh7@naver.com>
2024-11-06 17:21:52 +0900

    add: Account, Member 서비스 계층 단위(Unit) 테스트 추가

--- src/main/java/com/edio/user/controller/AccountController.java
+++ src/main/java/com/edio/user/controller/AccountController.java
@@ -1,7 +1,6 @@
 package com.edio.user.controller;
 
 import com.edio.common.model.response.SwaggerCommonResponses;
-import com.edio.studywithcard.card.service.CardService;
 import com.edio.user.domain.Accounts;
 import com.edio.user.model.reponse.AccountResponse;
 import com.edio.user.service.AccountService;
@@ -9,8 +8,6 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "Accounts", description = "Accounts 관련 API")
@@ -28,13 +25,13 @@ public class AccountController {
 
     @GetMapping("/account")
     @Operation(summary = "Account 정보 조회", description = "Account 정보를 조회합니다.")
-    public AccountResponse getAccount(@Parameter(required = true, description = "사용자 아이디") String loginId){
+    public AccountResponse getAccount(@Parameter(required = true, description = "사용자 아이디") String loginId) {
         return accountService.findOneAccount(loginId);
     }
 
-    @PostMapping("/account/create")
-    @Operation(summary = "Account 둥록", description = "Account를 등록합니다.")
-    public AccountResponse createAccount(@RequestBody Accounts account){
+    @PostMapping("/account")
+    @Operation(summary = "Account 등록", description = "Account를 등록합니다.")
+    public AccountResponse createAccount(@RequestBody Accounts account) {
         return accountService.createAccount(account);
     }
 
