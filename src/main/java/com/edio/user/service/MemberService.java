1a5de2a47c72bc0c62e4120735f4198b698a40aa
Kyeongho <43262369+GuardJo@users.noreply.github.com>
2024-11-08 19:12:08 +0900

    Revert "add: Account, Member 서비스 계층 단위(Unit) 테스트 추가"
    This reverts commit f50b7dc0b92aa00ea6ecb91cb2c7c307ac08432b.

--- src/main/java/com/edio/user/service/MemberService.java
+++ src/main/java/com/edio/user/service/MemberService.java
@@ -1,15 +1,11 @@
 package com.edio.user.service;
 
-import com.edio.user.domain.Accounts;
-import com.edio.user.domain.Members;
-import com.edio.user.model.reponse.AccountResponse;
-import com.edio.user.model.reponse.MemberResponse;
-
-import java.util.Optional;
+import com.edio.user.model.request.MemberCreateRequest;
+import com.edio.user.model.response.MemberResponse;
 
 public interface MemberService {
     // Member 조회
     MemberResponse findOneMember(long accountId);
     // Member 등록
-    MemberResponse createMember(Members member);
+    MemberResponse createMember(MemberCreateRequest memberCreateRequest);
 }
