dc2ba29a1f8f858fb559eff662ee658ed7ca39fb
YUNHWAN <629jyh7@naver.com>
2024-11-10 14:02:48 +0900

    feat: BaseException main브랜치와 동기화

--- src/main/java/com/edio/common/exception/BaseException.java
+++ src/main/java/com/edio/common/exception/BaseException.java
@@ -4,14 +4,14 @@ import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
 @Getter
-public class BaseException extends RuntimeException{
+public class BaseException extends RuntimeException {
 
     private HttpStatus status;
     private boolean isSuccess;
     private String detailMessage;
 
-    public BaseException(HttpStatus status, String message){
-        //fillInStackTrace();
+    public BaseException(HttpStatus status, String message) {
+//        fillInStackTrace();
         super(message);  // RuntimeException의 메시지 필드 설정
         this.status = status;
         this.isSuccess = false;
