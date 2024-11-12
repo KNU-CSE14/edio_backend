5f7660ffb7d07bc750721fbb1336d6dd1db1177c
YUNHWAN <629jyh7@naver.com>
2024-11-12 16:45:19 +0900

    add: profile local-prod 분리 작업

--- src/main/java/com/edio/EdioBackendApplication.java
+++ src/main/java/com/edio/EdioBackendApplication.java
@@ -11,20 +11,22 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 public class EdioBackendApplication {
 
-	public static void main(String[] args) {
-		// .env 파일 로드
-		Dotenv dotenv = Dotenv.load();
+    public static void main(String[] args) {
+        // .env 파일 로드
+        Dotenv dotenv = Dotenv.load();
 
-		// 환경 변수 값으로 시스템 속성 설정
-		System.setProperty("spring.datasource.username", dotenv.get("DB_USERNAME"));
-		System.setProperty("spring.datasource.password", dotenv.get("DB_PASSWORD"));
-		System.setProperty("spring.datasource.url", dotenv.get("DB_URL"));
+        // 환경 변수 값으로 시스템 속성 설정
+        System.setProperty("spring.datasource.username", dotenv.get("DB_USERNAME"));
+        System.setProperty("spring.datasource.password", dotenv.get("DB_PASSWORD"));
+        System.setProperty("spring.datasource.url", dotenv.get("DB_URL"));
 
-		System.setProperty("google.client-id", dotenv.get("GOOGLE_CLIENT_ID"));
-		System.setProperty("google.client-secret", dotenv.get("GOOGLE_CLIENT_SECRET"));
+        System.setProperty("google.client-id", dotenv.get("GOOGLE_CLIENT_ID"));
+        System.setProperty("google.client-secret", dotenv.get("GOOGLE_CLIENT_SECRET"));
 
-		System.setProperty("jwt.secret", dotenv.get("JWT_SECRET"));
+        System.setProperty("jwt.secret", dotenv.get("JWT_SECRET"));
 
-		SpringApplication.run(EdioBackendApplication.class, args);
-	}
+        System.setProperty("spring.profiles.active", dotenv.get("SPRING_PROFILES_ACTIVE"));
+
+        SpringApplication.run(EdioBackendApplication.class, args);
+    }
 }
