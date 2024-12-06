b948987e501683aa9b30b74c8de07f8567413adb
YUNHWAN <629jyh7@naver.com>
2024-12-06 14:51:45 +0900

    feat: 폴더 이동 예외 상황 처리

--- src/main/java/com/edio/studywithcard/folder/model/request/FolderCreateRequest.java
+++ src/main/java/com/edio/studywithcard/folder/model/request/FolderCreateRequest.java
@@ -1,7 +1,11 @@
 package com.edio.studywithcard.folder.model.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
+@AllArgsConstructor
 @Data
 public class FolderCreateRequest {
     private Long parentId;
