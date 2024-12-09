392d8c56cffb562958a75587b1b0ea55e542147c
YUNHWAN <629jyh7@naver.com>
2024-12-09 17:37:57 +0900

    feat: 토큰 로그 확인(테스트)

--- src/main/java/com/edio/common/security/OAuth2SuccessHandler.java
+++ src/main/java/com/edio/common/security/OAuth2SuccessHandler.java
@@ -37,6 +37,9 @@ public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
         String accessToken = jwtToken.getAccessToken();
         String refreshToken = jwtToken.getRefreshToken();
 
+        log.info(accessToken);
+        log.info(refreshToken);
+
         // 쿠키 생성 및 설정
         String accessTokenCookie = String.format("accessToken=%s; HttpOnly; Secure; Path=/; Max-Age=3600; SameSite=None", accessToken);
         String refreshTokenCookie = String.format("refreshToken=%s; HttpOnly; Secure; Path=/; Max-Age=86400; SameSite=None", refreshToken);
