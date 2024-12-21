dcaecb3d751cb5b56a379ff5dfd44326515bdd66
Kyeongho <dagh1218@naver.com>
2024-12-21 18:51:10 +0900

    [fix] #69 Swagger 내 API 호출 URL 변경
    - ec2 고정에서 현재 구동 서버를 기반으로 호출하도록 변경

--- src/main/java/com/edio/common/config/SwaggerConfig.java
+++ src/main/java/com/edio/common/config/SwaggerConfig.java
@@ -21,7 +21,7 @@ import org.springframework.context.annotation.Configuration;
         ),
         security = @SecurityRequirement(name = "bearerAuth"),
         servers = {
-                @Server(url = "https://ec2-3-38-251-128.ap-northeast-2.compute.amazonaws.com",
+                @Server(url = "/",
                         description = "서버 URL"),
         }
 )
