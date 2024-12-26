f3fc1371481b8c25fee1022d5903b69215732b03
YUNHWAN <629jyh7@naver.com>
2024-12-26 15:54:10 +0900

    feat: UnprocessableException을 entity 종속으로 수정

--- src/main/java/com/edio/common/exception/UnprocessableException.java
+++ src/main/java/com/edio/common/exception/UnprocessableException.java
@@ -1,13 +1,14 @@
 package com.edio.common.exception;
 
+import com.edio.common.domain.BaseEntity;
 import org.springframework.http.HttpStatus;
 
 public class UnprocessableException extends BaseException {
-    public <T> UnprocessableException(Class<T> entityClass, Long id) {
+    public <T extends BaseEntity> UnprocessableException(Class<T> entityClass, Long id) {
         super(HttpStatus.UNPROCESSABLE_ENTITY, String.format("%s UnProcessable entity: %d", entityClass.getSimpleName(), id));
     }
 
-    public <T> UnprocessableException(Class<T> entityClass, Object message) {
+    public <T extends BaseEntity> UnprocessableException(Class<T> entityClass, Object message) {
         super(HttpStatus.UNPROCESSABLE_ENTITY, String.format("%s UnProcessable entity: %s", entityClass.getSimpleName(), message));
     }
 }
