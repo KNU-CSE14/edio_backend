e612a61924b94cf2af903079a123015c79327965
YUNHWAN <629jyh7@naver.com>
2024-12-30 15:08:52 +0900

    feat: 주석 및 메서드명 refactor

--- src/main/java/com/edio/studywithcard/attachment/service/AttachmentService.java
+++ src/main/java/com/edio/studywithcard/attachment/service/AttachmentService.java
@@ -9,11 +9,15 @@ import java.io.IOException;
 import java.util.List;
 
 public interface AttachmentService {
+    // 첨부 파일 저장
     Attachment saveAttachment(MultipartFile file, String folder, String target) throws IOException;
 
+    // Deck 중간 테이블 저장
     void saveAttachmentDeckTarget(Attachment attachment, Deck deck);
 
+    // Card 중간 테이블 저장
     void saveAttachmentCardTarget(Attachment attachment, Card card);
 
-    void deleteAttachmentsBulk(List<String> fileKeys);
+    // 첨부 파일 삭제(Bulk)
+    void deleteAllAttachments(List<String> fileKeys);
 }
