3c69512ed3dc904bb3d2ff632e9a7788661236a7
YUNHWAN <629jyh7@naver.com>
2024-12-31 12:14:02 +0900

    feat: Folder 메서드 예외 처리 공통 ErrorMessage 처리

--- src/main/java/com/edio/common/exception/custom/ConflictException.java
+++ src/main/java/com/edio/common/exception/custom/ConflictException.java
@@ -2,14 +2,15 @@ package com.edio.common.exception.custom;
 
 import com.edio.common.domain.BaseEntity;
 import com.edio.common.exception.base.BaseException;
+import com.edio.common.exception.base.ErrorMessages;
 import org.springframework.http.HttpStatus;
 
 public class ConflictException extends BaseException {
     public <T extends BaseEntity> ConflictException(Class<T> entityClass, Long id) {
-        super(HttpStatus.CONFLICT, String.format("%s not found: %d", entityClass.getSimpleName(), id));
+        super(HttpStatus.CONFLICT, String.format("%s " + ErrorMessages.CONFLICT + ": %d", entityClass.getSimpleName(), id));
     }
 
     public <T extends BaseEntity> ConflictException(Class<T> entityClass, Object message) {
-        super(HttpStatus.CONFLICT, String.format("%s not found: %s", entityClass.getSimpleName(), message));
+        super(HttpStatus.CONFLICT, String.format("%s " + ErrorMessages.CONFLICT + ": %s", entityClass.getSimpleName(), message));
     }
 }
