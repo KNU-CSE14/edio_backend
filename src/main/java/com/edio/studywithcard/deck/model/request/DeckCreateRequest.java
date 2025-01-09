ed4c33b3330f184808639b3b48571aeebb83de9e
YUNHWAN <629jyh7@naver.com>
2025-01-09 14:04:33 +0900

    add: Swagger Multipart 처리를 위한 body 인터페이스 생성 및 적용

--- src/main/java/com/edio/studywithcard/deck/model/request/DeckCreateRequest.java
+++ src/main/java/com/edio/studywithcard/deck/model/request/DeckCreateRequest.java
@@ -1,5 +1,16 @@
 package com.edio.studywithcard.deck.model.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "Deck 생성 요청 정보", example = """
+        {
+          "folderId": 1,
+          "categoryId": 1,
+          "name": "Deck Name",
+          "description": "Deck Description",
+          "isShared": false
+        }
+        """)
 public record DeckCreateRequest(
         Long folderId,
         Long categoryId,
--- src/main/java/com/edio/studywithcard/deck/model/request/DeckMoveRequest.java
+++ src/main/java/com/edio/studywithcard/deck/model/request/DeckMoveRequest.java
@@ -1,5 +1,13 @@
 package com.edio.studywithcard.deck.model.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "Deck 이동 요청 정보", example = """
+        {
+          "id": 1,
+          "parentId": 2
+        }
+        """)
 public record DeckMoveRequest(
         Long id,
         Long parentId
