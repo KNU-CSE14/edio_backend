8c415304ebc5f2e98d529da8d4cc8f17d88a5fff
YUNHWAN <629jyh7@naver.com>
2025-02-10 17:00:11 +0900

    feat: Wrapper 제거 및 타입 변환 적용(String -> List)

--- src/main/java/com/edio/studywithcard/card/model/request/CardCreateOrUpdateRequest.java
+++ src/main/java/com/edio/studywithcard/card/model/request/CardCreateOrUpdateRequest.java
@@ -1,13 +1,9 @@
 package com.edio.studywithcard.card.model.request;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
-public class CardCreateOrUpdateRequest {
-    private Long deckId;
-    private Long cardId;
-    private String name;
-    private String description;
+public record CardCreateOrUpdateRequest(
+        Long deckId,
+        Long cardId,
+        String name,
+        String description
+) {
 }
