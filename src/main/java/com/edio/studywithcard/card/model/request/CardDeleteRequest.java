d746bfa609b13dac2e7bc9cc3069105dfc140520
YUNHWAN <629jyh7@naver.com>
2025-02-11 13:51:22 +0900

    feat: 단일 처리 클래스 제거 및 다중 카드 삭제 추가

--- src/main/java/com/edio/studywithcard/card/model/request/CardDeleteRequest.java
+++
@@ -1,6 +0,0 @@
-package com.edio.studywithcard.card.model.request;
-
-public record CardDeleteRequest(
-        Long id
-) {
-}
--- src/main/java/com/edio/studywithcard/card/service/CardServiceImpl.java
+++ src/main/java/com/edio/studywithcard/card/service/CardServiceImpl.java
@@ -7,12 +7,11 @@ import com.edio.studywithcard.attachment.domain.enums.AttachmentFolder;
 import com.edio.studywithcard.attachment.domain.enums.FileTarget;
 import com.edio.studywithcard.attachment.service.AttachmentService;
 import com.edio.studywithcard.card.domain.Card;
-import com.edio.studywithcard.card.model.request.*;
-import com.edio.studywithcard.card.model.response.CardResponse;
+import com.edio.studywithcard.card.model.request.CardBulkRequest;
+import com.edio.studywithcard.card.model.request.CardBulkRequestWrapper;
 import com.edio.studywithcard.card.repository.CardRepository;
 import com.edio.studywithcard.deck.domain.Deck;
 import com.edio.studywithcard.deck.repository.DeckRepository;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -38,88 +37,12 @@ public class CardServiceImpl implements CardService {
 
     private final AttachmentService attachmentService;
 
-    private final ObjectMapper objectMapper;
-
-    /*
-        카드 생성
-     */
-    @Override
-    @Transactional
-    public CardResponse createCard(CardCreateRequest request, MultipartFile[] files) {
-        try {
-            // Deck 조회
-            Deck deck = deckRepository.findById(request.deckId())
-                    .orElseThrow(() -> new EntityNotFoundException(ErrorMessages.NOT_FOUND_ENTITY.format(Deck.class.getSimpleName(), request.deckId())));
-
-            // 1. Card 생성 및 저장
-            Card card = Card.builder()
-                    .deck(deck)
-                    .name(request.name())
-                    .description(request.description())
-                    .build();
-            cardRepository.save(card);
-
-            // 2. 첨부파일 처리
-            for (MultipartFile file : files) {
-                if (file != null && !file.isEmpty()) {
-                    processAttachment(file, card);
-                }
-            }
-            return CardResponse.from(card);
-        } catch (IOException e) {
-            log.error(e.getMessage());
-            throw new IllegalStateException(ErrorMessages.FILE_PROCESSING_ERROR.getMessage()); // 422
-        }
-    }
-
-    /*
-        카드 수정
-     */
-    @Override
-    @Transactional
-    public void updateCard(CardUpdateRequest request, MultipartFile[] files) {
-        Card existingCard = cardRepository.findByIdAndIsDeletedFalse(request.id())
-                .orElseThrow(() -> new EntityNotFoundException(ErrorMessages.NOT_FOUND_ENTITY.format(Card.class.getSimpleName(), request.id())));
-
-        // 카드 이름
-        if (StringUtils.hasText(request.name())) {
-            existingCard.setName(request.name());
-        }
-        // 카드 설명
-        if (StringUtils.hasText(request.description())) {
-            existingCard.setDescription(request.description());
-        }
-
-        // 첨부파일 수정
-        for (MultipartFile file : files) {
-            if (file != null && !file.isEmpty()) {
-                try {
-                    // 기존 첨부파일 삭제(Bulk 작업)
-                    List<String> fileKeys = existingCard.getAttachmentCardTargets().stream()
-                            .map(AttachmentCardTarget::getAttachment)
-                            .filter(attachment -> !attachment.isDeleted())
-                            .map(Attachment::getFileKey)
-                            .collect(Collectors.toList());
-
-                    if (!fileKeys.isEmpty()) {
-                        attachmentService.deleteAllAttachments(fileKeys);
-                    }
-
-                    processAttachment(file, existingCard);
-                } catch (IOException e) {
-                    log.error(e.getMessage());
-                    throw new IllegalStateException(ErrorMessages.FILE_PROCESSING_ERROR.getMessage()); // 422
-                }
-            }
-        }
-    }
-
     /*
         카드 생성 or 수정
      */
     @Override
     @Transactional
-    public void createOrUpdateCard(CardBulkRequestWrapper cardBulkRequestWrapper) {
+    public void createOrUpdateCards(CardBulkRequestWrapper cardBulkRequestWrapper) {
 
         for (CardBulkRequest request : cardBulkRequestWrapper.getRequests()) {
             log.info("[Test] bulkRequest : {}", request);
@@ -187,26 +110,28 @@ public class CardServiceImpl implements CardService {
     }
 
     /*
-        카드 삭제
-    */
+       카드 삭제
+   */
     @Override
     @Transactional
-    public void deleteCard(CardDeleteRequest request) {
-        Card existingCard = cardRepository.findByIdAndIsDeletedFalse(request.id())
-                .orElseThrow(() -> new EntityNotFoundException(ErrorMessages.NOT_FOUND_ENTITY.format(Card.class.getSimpleName(), request.id())));
+    public void deleteCards(List<Long> request) {
+        for (long cardId : request) {
+            Card existingCard = cardRepository.findByIdAndIsDeletedFalse(cardId)
+                    .orElseThrow(() -> new EntityNotFoundException(ErrorMessages.NOT_FOUND_ENTITY.format(Card.class.getSimpleName(), cardId)));
 
-        // Bulk 작업
-        List<String> fileKeys = existingCard.getAttachmentCardTargets().stream()
-                .map(AttachmentCardTarget::getAttachment)
-                .filter(attachment -> !attachment.isDeleted())
-                .map(Attachment::getFileKey)
-                .collect(Collectors.toList());
+            // Bulk 작업
+            List<String> fileKeys = existingCard.getAttachmentCardTargets().stream()
+                    .map(AttachmentCardTarget::getAttachment)
+                    .filter(attachment -> !attachment.isDeleted())
+                    .map(Attachment::getFileKey)
+                    .collect(Collectors.toList());
 
-        if (!fileKeys.isEmpty()) {
-            attachmentService.deleteAllAttachments(fileKeys);
+            if (!fileKeys.isEmpty()) {
+                attachmentService.deleteAllAttachments(fileKeys);
+            }
+
+            existingCard.setDeleted(true);
         }
-
-        existingCard.setDeleted(true);
     }
 
 
