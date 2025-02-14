ab488c51a52b39be58e1c0b59b9ed25f1ed0c705
YUNHWAN <629jyh7@naver.com>
2025-02-14 17:59:20 +0900

    feat: 카드 등록/수정 서비스 로직 메서드 분리 - 등록, 수정 각각 메서드 로직 분리 - accountId를 사용하여 등록, 수정 권한 가능한 사용자인지 검사

--- src/main/java/com/edio/studywithcard/card/controller/CardController.java
+++ src/main/java/com/edio/studywithcard/card/controller/CardController.java
@@ -1,10 +1,12 @@
 package com.edio.studywithcard.card.controller;
 
+import com.edio.common.security.CustomUserDetails;
 import com.edio.studywithcard.card.model.request.CardBulkRequestWrapper;
 import com.edio.studywithcard.card.service.CardService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,8 +21,9 @@ public class CardController implements CardApiDoc {
 
     @PostMapping(value = "/cards", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     @Override
-    public void createOrUpdateCards(@ModelAttribute CardBulkRequestWrapper cardBulkRequestWrapper) {
-        cardService.createOrUpdateCards(cardBulkRequestWrapper);
+    public void createOrUpdateCards(@AuthenticationPrincipal CustomUserDetails userDetails,
+                                    @ModelAttribute CardBulkRequestWrapper cardBulkRequestWrapper) {
+        cardService.createOrUpdateCards(userDetails.getAccountId(), cardBulkRequestWrapper);
     }
 
     @DeleteMapping("/cards")
