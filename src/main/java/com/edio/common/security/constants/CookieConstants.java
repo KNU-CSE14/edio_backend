238e3d899aa6f35ca95cb8cd55e3a27ea43d0d37
YUNHWAN <629jyh7@naver.com>
2025-02-14 16:34:57 +0900

    feat: SonarQube 활용 코드 리팩토링

--- src/main/java/com/edio/common/security/constants/CookieConstants.java
+++ src/main/java/com/edio/common/security/constants/CookieConstants.java
@@ -1,8 +1,9 @@
 package com.edio.common.security.constants;
 
+import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class CookieConstants {
     public static final String COOKIE_ACCESS_TOKEN = "accessToken=%s; HttpOnly; Secure; Path=/; Max-Age=3600; SameSite=None";
     public static final String COOKIE_REFRESH_TOKEN = "refreshToken=%s; HttpOnly; Secure; Path=/; Max-Age=86400; SameSite=None";
