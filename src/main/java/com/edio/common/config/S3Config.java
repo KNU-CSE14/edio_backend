701b16e2d301a6cd5ea1a6992e1ef4019a3f895b
YUNHWAN <629jyh7@naver.com>
2025-02-27 14:25:35 +0900

    feat: #101 프로젝트 전체적인 코드 리팩토링 진행 - yml파일 공통, local, prod에 따른 명확한 분리 - 로컬, 운영 환경에서 환경 변수 주입이 동일한 코드로 적용될 수 있도록 @Value 사용 - 운영 build시에 env설정 방식 변경 예정

--- src/main/java/com/edio/common/config/S3Config.java
+++ src/main/java/com/edio/common/config/S3Config.java
@@ -1,5 +1,6 @@
 package com.edio.common.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
@@ -9,15 +10,25 @@ import software.amazon.awssdk.services.s3.S3Client;
 
 @Configuration
 public class S3Config {
+
+    @Value("${AWS_REGION}")
+    private String region;
+
+    @Value("${AWS_ACCESS_KEY_ID}")
+    private String accessKey;
+
+    @Value("${AWS_SECRET_KEY_ID}")
+    private String secretKey;
+
     @Bean
     public S3Client s3Client() {
         return S3Client.builder()
-                .region(Region.of(System.getProperty("AWS_REGION")))
+                .region(Region.of(region))
                 .credentialsProvider(
                         StaticCredentialsProvider.create(
                                 AwsBasicCredentials.create(
-                                        System.getProperty("AWS_ACCESS_KEY_ID"),
-                                        System.getProperty("AWS_SECRET_KEY_ID")
+                                        accessKey,
+                                        secretKey
                                 )
                         )
                 )
