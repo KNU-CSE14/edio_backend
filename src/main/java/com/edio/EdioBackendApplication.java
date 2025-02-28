fe4573a8cf5b70d32a021c33889f3555b9596e2b
YUNHWAN <629jyh7@naver.com>
2025-02-28 14:15:06 +0900

    feat: env 세팅 수정

--- src/main/java/com/edio/EdioBackendApplication.java
+++ src/main/java/com/edio/EdioBackendApplication.java
@@ -26,23 +26,23 @@ public class EdioBackendApplication {
     public static void main(String[] args) {
         // 운영환경에서는 시스템 환경변수를 우선 사용하고, 로컬인 경우에만 .env 파일 로드
         String profile = System.getenv("SPRING_PROFILES_ACTIVE");
-        if(profile == null || profile.isEmpty()){
-            Dotenv dotenv = Dotenv.load();
-            profile = dotenv.get("SPRING_PROFILES_ACTIVE");
-        }
-        System.setProperty("spring.profiles.active", profile);
 
         SpringApplication app = new SpringApplication(EdioBackendApplication.class);
-        // local 프로파일일 때만 .env 파일의 변수들을 Spring Environment에 추가
-        if("local".equals(profile)){
+
+        if(profile == null || profile.isEmpty()){
+            Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+            profile = dotenv.get("SPRING_PROFILES_ACTIVE");
+
             app.addInitializers(context -> {
                 ConfigurableEnvironment env = context.getEnvironment();
                 Map<String, Object> envVars = new HashMap<>();
-                Dotenv dotenv = Dotenv.load();
                 dotenv.entries().forEach(entry -> envVars.put(entry.getKey(), entry.getValue()));
                 env.getPropertySources().addFirst(new org.springframework.core.env.MapPropertySource("dotenvProperties", envVars));
             });
         }
+
+        System.setProperty("spring.profiles.active", profile);
+
         app.run(args);
     }
 }
