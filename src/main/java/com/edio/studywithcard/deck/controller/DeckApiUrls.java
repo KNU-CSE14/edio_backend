898b4341c9d6398d230c1c15862c677f25c8efbf
Kyeongho <dagh1218@naver.com>
2025-03-03 15:41:14 +0900

    fix: #98 피드백 반영
    - Deck 관련 API url 오타 수정

--- src/main/java/com/edio/studywithcard/deck/controller/DeckApiUrls.java
+++ src/main/java/com/edio/studywithcard/deck/controller/DeckApiUrls.java
@@ -6,5 +6,5 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class DeckApiUrls {
     public final static String DECK_URL = "/api/deck";
-    public final static String DECK_POSITION_URL = "/deck/position";
+    public final static String DECK_POSITION_URL = "/api/deck/position";
 }
