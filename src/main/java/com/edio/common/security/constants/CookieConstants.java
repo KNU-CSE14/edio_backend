bed2e433ffc8e198a2618710ade02afa72ca7c8f
정윤환 <629jyh7@naver.com>
2025-04-17 13:43:40 +0900

    feat: cookie domain 수정(edio.shop)

--- src/main/java/com/edio/common/security/constants/CookieConstants.java
+++ src/main/java/com/edio/common/security/constants/CookieConstants.java
@@ -5,6 +5,6 @@ import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class CookieConstants {
-    public static final String COOKIE_ACCESS_TOKEN = "accessToken=%s; HttpOnly; Secure; Path=/; Max-Age=3600; SameSite=None; Domain=ec2-3-38-251-128.ap-northeast-2.compute.amazonaws.com";
-    public static final String COOKIE_REFRESH_TOKEN = "refreshToken=%s; HttpOnly; Secure; Path=/; Max-Age=86400; SameSite=None; Domain=ec2-3-38-251-128.ap-northeast-2.compute.amazonaws.com";
+    public static final String COOKIE_ACCESS_TOKEN = "accessToken=%s; HttpOnly; Secure; Path=/; Max-Age=3600; SameSite=None; Domain=.edio.shop";
+    public static final String COOKIE_REFRESH_TOKEN = "refreshToken=%s; HttpOnly; Secure; Path=/; Max-Age=86400; SameSite=None; Domain=.edio.shop";
 }
